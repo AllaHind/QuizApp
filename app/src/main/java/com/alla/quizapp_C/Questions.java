848b547be214a1caf9c559c751e35c8fe202291f
Hind Alla <iamhiindou@gmail.com>
2022-04-13 12:39:43 +0000

    Initial commit

---
+++ app/src/main/java/com/alla/quizapp_C/Questions.java
@@ -0,0 +1,30 @@
+package com.alla.quizapp_C;
+
+
+public class Questions {
+    public String Truevalue;
+    public String falsevalue;
+    public String Question;
+    public String image;
+
+
+    public Questions(){
+
+    }
+
+
+    public Questions(String Question,String Truevalue, String falsevalue,String image){
+
+
+
+        this.Question=Question;
+        this.Truevalue = Truevalue;
+        this.falsevalue =  falsevalue;
+        this.image=image;
+
+    }
+
+}
+
+
+
---
+++ app/src/main/java/com/alla/quizapp_C/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.alla.quizapp_C;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    Button startbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startbtn=(Button)findViewById(R.id.start_btn);
+        startbtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
